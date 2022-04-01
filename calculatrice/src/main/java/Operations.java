cbee15b883e02a42ff7fa9f0e45ba3bfeba63510
sabir.ilyass <ilyasssabir7@gmail.com>
2022-04-01 03:18:43 +0200

    add all files of the project

---
+++ calculatrice/src/main/java/Operations.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author sabir ilyass
+ */
+public enum Operations {
+    add,
+    sub,
+    multiplication,
+    division,
+    modulo,
+    power;  
+}
